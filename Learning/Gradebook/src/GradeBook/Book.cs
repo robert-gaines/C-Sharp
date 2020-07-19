92467335f9221eda25e174530a61cd35872c559d
Robert W. Gaines <robert.gaines@homelab.winlab.local>
2020-07-19 14:24:12 -0700

    Added methods to Book class

--- Learning/Gradebook/src/GradeBook/Book.cs
+++ Learning/Gradebook/src/GradeBook/Book.cs
@@ -11,11 +11,69 @@ namespace GradeBook
             grades = new List<double>();
             this.name = name;
         }
+
+        public void PopulateBook()
+        {
+            var rand_value = new Random();
+
+            for(int i = 0; i <= 10; i++)
+            {
+                var random_entry = rand_value.Next(100);
+                grades.Add(random_entry);
+            }
+        }
+
+        public void DisplayBook()
+        {
+            System.Console.WriteLine("[*] Displaying grades...");
+            foreach(double grade in grades)
+            {
+                System.Console.WriteLine(grade);
+            }
+        }
+        public double FindMin()
+        {
+            double min_value = grades[0];
+            foreach(double grade in grades)
+            {
+                if(grade < min_value)
+                {
+                    min_value = grade;
+                }
+            }
+            return min_value;
+        }
+        public double FindMax()
+        {   
+            double max_value = 0;
+            foreach(double grade in grades)
+            {
+                if(grade > max_value)
+                {
+                    max_value = grade;
+                }    
+            }
+            return max_value;
+        }
+
+        public double FindAverage()
+        {
+            double sum = 0;
+            double avg = 0;
+
+            foreach(double grade in grades)
+            {
+                sum += grade;
+            }
+
+            avg = sum/grades.Count;
+
+            return avg;
+        }
         public void AddGrade(double gradeEntry)
         {
             grades.Add(gradeEntry);
         }
-
         private List<double> grades;
         private string name;
     }
--- Learning/Gradebook/src/GradeBook/Program.cs
+++ Learning/Gradebook/src/GradeBook/Program.cs
@@ -44,59 +44,75 @@ namespace GradeBook
 
             var book = new Book("Test");
 
-            book.AddGrade(89.1);
+            book.PopulateBook();
 
-            var values = new double[100];
+            book.DisplayBook();
 
-            var rand_value = new Random();
+            double max_value;
+            double min_value;
+            double book_avg;
+
+            max_value = book.FindMax();
+            min_value = book.FindMin();
+
+            book_avg = book.FindAverage();
+
+            System.Console.WriteLine("[*] Max Value: "+max_value);
+            System.Console.WriteLine("[*] Min Value: "+min_value);
+
+            System.Console.WriteLine("[*] Book Average: "+book_avg);
+
+            // var values = new double[100];
+
+            // var rand_value = new Random();
 
             // System.Console.WriteLine(rand_value.Next(100));
 
-            var values_length = values.Length;
+            // var values_length = values.Length;
 
-            for(int i = 0;i < values_length; i++)
-            {
-                var random_entry = rand_value.Next(100);
-                System.Console.WriteLine($"[+] Adding: {random_entry}");
-                values[i] += random_entry;
-            }
+            // for(int i = 0;i < values_length; i++)
+            // {
+            //     var random_entry = rand_value.Next(100);
+            //     System.Console.WriteLine($"[+] Adding: {random_entry}");
+            //     values[i] += random_entry;
+            // }
 
-            double sum = 0;
+            // double sum = 0;
 
-            for(int j = 0; j < values_length; j++)
-            {
-                sum += values[j];
-            }
+            // for(int j = 0; j < values_length; j++)
+            // {
+            //     sum += values[j];
+            // }
 
-            System.Console.WriteLine($"[*] Sum of the values array is: {sum}");
+            // System.Console.WriteLine($"[*] Sum of the values array is: {sum}");
 
-            double average = sum / values.Length;
+            // double average = sum / values.Length;
 
-            System.Console.WriteLine($"[*] Average of elements in the values array is: {average}");
+            // System.Console.WriteLine($"[*] Average of elements in the values array is: {average}");
 
-            var alt_values = new List<double>();
+            // var alt_values = new List<double>();
 
-            int k = 100; int l = 0;
-            var rand_alt = new Random();
-            while(l < k)
-            {
-                alt_values.Add(rand_alt.Next(100));
-                l++;
-            }
+            // int k = 100; int l = 0;
+            // var rand_alt = new Random();
+            // while(l < k)
+            // {
+            //     alt_values.Add(rand_alt.Next(100));
+            //     l++;
+            // }
 
-            double list_sum = 0;
+            // double list_sum = 0;
 
-            foreach(double value in alt_values)
-            {
-                System.Console.WriteLine($"[*] Values List Element: {value}");
-                list_sum += value;
-            }
+            // foreach(double value in alt_values)
+            // {
+            //     System.Console.WriteLine($"[*] Values List Element: {value}");
+            //     list_sum += value;
+            // }
 
-            double list_length = alt_values.Count;
-            System.Console.WriteLine($"[*] List length is: {list_length} ");
-            System.Console.WriteLine($"[*] Sum of list values is: {list_sum}");
-            double list_average = list_sum / list_length;
-            System.Console.WriteLine($"[*] Average of list values is: {list_average:N3} ");
+            // double list_length = alt_values.Count;
+            // System.Console.WriteLine($"[*] List length is: {list_length} ");
+            // System.Console.WriteLine($"[*] Sum of list values is: {list_sum}");
+            // double list_average = list_sum / list_length;
+            // System.Console.WriteLine($"[*] Average of list values is: {list_average:N3} ");
    
         }
     }
